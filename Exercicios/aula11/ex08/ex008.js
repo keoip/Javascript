4487c5719d39a36434241186bcb9b7c16039232c
Kelvin melo <kelvinhenriquemelo@gmail.com>
2024-03-14 19:11:59 -0300

    comited

---
+++ Exercicios/aula11/ex08/ex008.js
@@ -0,0 +1,10 @@
+var vel = 30
+
+console.log(`A velocidade do seu carro é de ${vel} KM/hr`)
+
+if(vel > 60){ // Condição Simples!
+    console.log('Voce ultrapassou os limites de velocidade. MULTADO!')
+}
+
+console.log("Digija sempre com cinto de segurança!")
+
---
+++ Exercicios/aula11/ex09/ex009.js
@@ -0,0 +1,7 @@
+var país = 'frança'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('Voce é brasileiro!')
+} else {
+    console.log('Voce é estrangeiro!')
+}
