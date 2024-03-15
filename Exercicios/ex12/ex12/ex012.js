993e3ce963e062b63d35ee561be2547df3f88463
Kelvin melo <kelvinhenriquemelo@gmail.com>
2024-03-14 23:24:01 -0300

    comited

--- Exercicios/ex12/ex12/ex012.js
+++ Exercicios/ex12/ex12/ex012.js
@@ -1,7 +1,7 @@
 var agora = new Date()
 var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas`)
-if(hora > 0 && hora < 5){
+if(hora > 0 && hora < 5){ // condição aninhada
     console.log('Boa madrugada!')
 }else if (hora < 12 && hora > 5){
     console.log('Bom dia!')
---
+++ Exercicios/ex12/ex13/ex013.js
@@ -0,0 +1,16 @@
+var agora = new Date()
+var Diasem = agora.getDay()
+/* 
+
+ 0 = domingo
+ 1 = segunda
+ 2 = terça
+ 3 = quarta
+ 4 = quinta 
+ 5 = sexta
+ 6 = sabado
+
+
+*/
+
+console.log(`${Diasem}`)
