0c3fd55cba14bcf939357839e74860b2fa58f1df
Kelvin melo <kelvinhenriquemelo@gmail.com>
2024-03-16 19:28:11 -0300

    comited

--- Exercicios/aula12/ex13/ex013.js
+++ Exercicios/aula12/ex13/ex013.js
@@ -1,5 +1,6 @@
 var agora = new Date()
 var Diasem = agora.getDay()
+
 /* 
 
  0 = domingo
@@ -13,4 +14,28 @@ var Diasem = agora.getDay()
 
 */
 
-console.log(`${Diasem}`)
+switch(Diasem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+         break
+    case 4:
+        console.log('Quinta')
+         break
+    case 5:
+        console.log('Sexta') 
+         break
+    case 6:
+        console.log('Sabado') 
+         break
+    default:
+        console.log('[ERRO] DIA INVALIDO!')      
+}
