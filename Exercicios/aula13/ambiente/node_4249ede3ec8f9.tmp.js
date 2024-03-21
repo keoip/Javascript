3809f89575d9c23fb25584d48b93787aed91f309
Kelvin melo <kelvinhenriquemelo@gmail.com>
2024-03-21 13:17:00 -0300

    COMITED

---
+++ Exercicios/aula13/ambiente/node_4249ede3ec8f9.tmp.js
@@ -0,0 +1,25 @@
+
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+     //Vai somar o 1 lá de cima toda vez que chegar nessa parte do codigo!
+} while( c <= 6)
+
+
+/* REPETIÇÃO COM TESTE LOGICO NO INICIO
+var c = 1
+while(c <= 6){
+    console.log(`Passo ${c}`)
+    c++ //Vai somar o 1 lá de cima toda vez que chegar nessa parte do codigo!
+}
+*/
+
+
+/* JEITO BURRO DE FAZER
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+*/
