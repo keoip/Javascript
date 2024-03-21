fa0e46c4eda0884f28ffe408a777d8aad7e758c3
Kelvin melo <kelvinhenriquemelo@gmail.com>
2024-03-21 13:51:08 -0300

    comited

---
+++ Exercicios/aula 14/ambiente/script.js
@@ -0,0 +1,22 @@
+for(var c = 10; c >= 0 ; c--){
+    console.log(`Passo ${c}`)
+}
+
+/* Teste logico no final
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+*/
+
+/* Teste logico no inicio
+
+var c = 1
+while(c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+*/
