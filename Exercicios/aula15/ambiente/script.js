76f6486d73e084ffc2e08e9de4403fc8e2d738f5
Kelvin melo <kelvinhenriquemelo@gmail.com>
2024-03-22 19:48:19 -0300

    comited

--- Exercicios/aula15/ambiente/script.js
+++ Exercicios/aula15/ambiente/script.js
@@ -1,5 +1,5 @@
 let valor = [5,9,4,7,3,6,1,2,8]
-valor.sort()
+valor.sort() // ORGANIZA A ARRAY DE FORMA CRESCENTE
 let v = valor.indexOf(5) // () SIGNIFICA O VALOR DENTRO DA ARRAY E NÃO O INDICE, A CHAVE NELE
 if(v == -1){
     console.log('Esse valor não existe')
@@ -7,29 +7,37 @@ if(v == -1){
     console.log(`Este valor está na posição ${v}`)
 }
 
+for(let c in valor){ // "FOR" PARA ARRAYS, SIMPLICANDO COLOCANDO SÓ DOIS(O COMEÇO E A ARRAY)
+    console.log(valor[c])
+}
 
+
+/* REPETIÇÃO COM TESTE LOGICO NO FINAL
+let c = 0
+do{
+    console.log(`O indice ${c} está na posição ${valor[c]}`)
+    c++
+} while(c < valor.length)
+*/
+
+
+
+
+
+/* REPETIÇÃO COM TESTE LOGICO NO COMEÇO
 let c = 0
 while(c <valor.length){
     console.log(`o indice ${c} está na posição ${valor[c]}`)
     c++
 }
+*/
 
 
 
 
 
-
-/*
+/* REPETIÇAÕ "FOR" QUE INCURTA O COMEÇO, TESTE LOGICO, INCREMENTO, TUDO NESSA ORDEM
 for(let c = 0; c < valor.length; c++){
     console.log(`O indice ${c} está na posição ${valor[c]}`)
 }
 */
-
-
-
-/*
-for(let c in valor){
-    console.log(valor[c])
-}
-*/
-
